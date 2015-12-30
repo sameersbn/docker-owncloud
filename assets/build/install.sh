f3cf32161ee60c59b1bc573c144fd9ef1f09f214
Sameer Naik <sameer@damagehead.com>
2015-12-30 21:26:09 +0530

    configuring `always_populate_raw_post_data = -1` is no longer required

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -12,10 +12,6 @@ echo "Extracting ownCloud ${OWNCLOUD_VERSION}..."
 tar -xf ${OWNCLOUD_BUILD_DIR}/owncloud-${OWNCLOUD_VERSION}.tar.bz2 --strip=1 -C ${OWNCLOUD_INSTALL_DIR}
 rm -rf ${OWNCLOUD_BUILD_DIR}/owncloud-${OWNCLOUD_VERSION}.tar.bz2
 
-# required by owncloud
-sed -i "s|[;]*[ ]*always_populate_raw_post_data = .*|always_populate_raw_post_data = -1|" /etc/php5/fpm/php.ini
-sed -i "s|[;]*[ ]*always_populate_raw_post_data = .*|always_populate_raw_post_data = -1|" /etc/php5/cli/php.ini
-
 # upload_max_filesize
 (
   echo "default_charset='UTF-8'"
